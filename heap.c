c83750de75279e14fe59be1d7948048f27f373b3
Replit user <>
2023-05-12 03:24:02 +0000

    new try

--- heap.c
+++ heap.c
@@ -18,10 +18,9 @@ typedef struct Heap{
 
 
 void* heap_top(Heap* pq){
-  if(pq->size==0) 
+  if(pq->size==0 || pq==NULL) 
     return NULL;
-  else
-    return pq->heapArray[0]->data;
+  else return pq->heapArray[0].data;
 }
 
 
@@ -39,7 +38,6 @@ Heap* createHeap(){
   Heap* aux =(Heap*) malloc(sizeof(Heap));
   aux->capac=3;
   aux->size=0;
-  aux->heapArray= (heapElem*) malloc(sizeof(heapElem)*aux->capac);
-  
+  aux->heapArray= (heapElem*) malloc(sizeof(heapElem)*aux->capac); 
   return aux;
 }
